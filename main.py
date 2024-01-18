e8ef0a4c6955e2e7a06c396ab239099de421a407
Harmen <harmen.kerkenaar@qbis.ch>
2024-01-18 09:57:43 +0100

    Initial commit, connect to google gemini api)

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY)
+
+tweet_prompt = PromptTemplate.from_template("""
+    You're customer service. Write me an acurate answer on the following question.
+    Do not make up answers, stick to the facts {question}.
+    """)
+
+tweet_chain = LLMChain(llm=llm, prompt=tweet_prompt, verbose=True)
+
+if __name__=="__main__":
+    question = ""
+    resp = tweet_chain.run(topic=topic)
+    print(resp)
