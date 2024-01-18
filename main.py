b9ea9a8f4466fa5df2d830ed7226e76e9e9d19a3
Harmen <harmen.kerkenaar@qbis.ch>
2024-01-18 14:37:20 +0100

    Replace gemini with OpenAI

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import os
 
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import OpenAI
+
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+API_ENDPOINT  = os.getenv("API_ENDPOINT ")
 
-llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY)
 
-tweet_prompt = PromptTemplate.from_template("""
+llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+
+customer_prompt = PromptTemplate.from_template("""
     You're customer service. Write me an acurate answer on the following question.
     Do not make up answers, stick to the facts {question}.
     """)
 
-customer_chain = LLMChain(llm=llm, prompt=tweet_prompt, verbose=True)
+customer_chain = LLMChain(llm=llm, prompt=customer_prompt, verbose=True)
 
 if __name__=="__main__":
     question = "What is 1+1?"
