6655fe6453e7309c77d94a4697ba6ba3dfa10164
Harmen <harmen.kerkenaar@qbis.ch>
2024-01-19 10:47:13 +0100

    Add SQL chain

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import os
+import json
+from pathlib import Path
 
 from dotenv import load_dotenv
 from langchain_openai import OpenAI
 
-from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.utilities import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-API_ENDPOINT  = os.getenv("API_ENDPOINT ")
+OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+API_ENDPOINT: str  = os.getenv("API_ENDPOINT ")
+
+base_path: Path = Path(__file__).parent
+config_path: Path = base_path / 'config.json'
+
+with open(config_path, encoding="utf-8") as f:
+    config: dict = json.load(f)
+    conf_db: dict = config['db']
+
+conn_str: str = f"postgresql+psycopg2://{conf_db['username']}:{conf_db['password']}@{conf_db['host']}/{conf_db['database']}"
+db = SQLDatabase.from_uri(
+    conn_str,
+    schema=conf_db['schema'],
+    include_tables=[
+        "calendar",
+        "sales",
+        "sell_price"
+
+    ],  # we include only one table to save tokens in the prompt :)
+    sample_rows_in_table_info=3
+    )
 
 
-llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+llm = OpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    model=config['model']['name'],
+    temperature=config['model']['temperature']
+    )
 
-customer_prompt = PromptTemplate.from_template("""
-    You're customer service. Write me an acurate answer on the following question.
-    Do not make up answers, stick to the facts {question}.
-    """)
+db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
 
-customer_chain = LLMChain(llm=llm, prompt=customer_prompt, verbose=True)
-
-if __name__=="__main__":
-    question = "What is 1+1?"
-    resp = customer_chain.run(question=question)
-    print(resp)
+db_chain.run("How many locations are there with more than 20 distinct products?")
